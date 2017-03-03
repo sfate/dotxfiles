fb011f66d65f019745098b577f6073cebb75b794
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2017-03-03 16:39:08 +0200

    update deploy script + readme

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-dotZ
+## [dotxfiles](https://github.com/sfate/dotxfiles) - small configuration for productive work in terminal
 ====
 
-Bunch of dotfiles for development.
+### Install or Update
+`$ curl -sLo- https://git.io/dotxfiles-deploy.sh | bash`
+##### *Note*: This will override your existing setup!
+
+### Versions
+All deployed versions and old configurations are stored at `~/.xfiles`.
 
 ## Requirements
-
 * Git (1.8+)
-* [Vim](https://github.com/sfate/Vim-environment) (7.3+)
-* [tmux](https://gist.github.com/sfate/4510955) (1.7+)
-* Ruby (1.8.7+)
-* RVM (1.21+)
+* [Vim](https://github.com/sfate/dotvimmy) (8.0+)
+* [tmux](https://github.com/sfate/dotxfiles/blob/master/dots/tmux.conf) (1.7+)
+* Ruby (2.0+)
 
+### Version 0.1.9 (03 Mar 2017)
--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,52 @@
-function set_progress() {
-  bar_symbols=""
-  tput sc
-  tput cuf 2; echo -n "[*] Copying:  [....................................................................................................] 0%";
-  tput rc
-  count=$([[ $1 -gt 100 ]] && echo 100 || echo $1)
-  for i in $(seq 1 $count); do
-    bar_symbols+="#"
-  done
-  tput sc
-  tput cuf 17; echo -n $bar_symbols
-  tput rc; tput sc
-  tput cuf 119; echo -n "$count%   "
-  tput rc
-  [[ $1 =~ 100 ]] && echo
-}
+# Maintainer: https://github.com/sfate
+# Source: https://github.com/sfate/dotxfiles
+# Version: 0.1.9 - 03 Mar 2017
+#
+# How_to_Install_or_Update:
+#    !NOTE: This will override your existing setup
+#    $ curl -Lo- https://git.io/dotxfiles-deploy.sh | bash
+set -e
 
-echo
-echo "Start dotfiles deploy:"
-cd $HOME
-(git clone https://github.com/sfate/dotZ.git dotZ --quiet) > /dev/null 2>&1
-FILES=($HOME/dotZ/dots/*)
-length=$((100/${#FILES[@]}))
-iter=0
+start_time=$(date +%s)
+release_name=$(date +"%Y%m%d%H%M%S")
+name='dotxfiles'
+config_dir=$HOME/.$dotxfiles
+releases_dir=$config_dir/releases
+backup_dir=$config_dir/backup
+current_release_dir=$releases_dir/$release_name
+current_backup_dir=$backup_dir/$release_name
+
+echo "[*] Downloaded deploy script for: https://github.com/sfate/$name"
+echo "[*] Start deploy of $name configuration"
+
+echo "[*] Clone $name repo"
+(git clone https://github.com/sfate/$name.git $current_release_dir --quiet) &> /dev/null
+FILES=($current_release_dir/dots/*)
+
+echo "[*] Old configuration backup"
+(mkdir -p $config_dir/{backup,releases}/$release_name)
+(mkdir -p $current_backup_dir/dots)
+(mv $HOME/.bin $current_backup_dir)
+(mv $HOME/.osx $current_backup_dir)
+for f in ${FILES[@]}; do
+  mv "$f" "$current_backup_dir/$(basename $f)"
+done
+
+echo "[*] Link folders"
+(ln -s $current_release_dir/bin $HOME/.bin)
+(ln -s $current_release_dir/osx $HOME/.osx)
+
+echo "[*] Place new configuration files"
 for f in ${FILES[@]}; do
   cp -r "$f" "$HOME/.$(basename $f)"
-
-  iter=$(($iter + 1))
-  set_progress $(echo $(($iter * $length)))
-  sleep 0.1
 done
-rm -rf dotZ
-set_progress 100
-echo "All done. Exiting.."
-echo
+
+echo "[*] Clean up"
+old_releases=$(ls -td $releases_dir/* | tail -n +6)
+old_backups=$(ls -td $backup_dir/* | tail -n +6)
+[ -n "$old_releases" ] && rm -rf $old_releases
+[ -n "$old_backups" ] && rm -rf $old_backups
+
+echo "[*] Deployed successfully. Time spent: $(($(date +%s)-$start_time))s"
+
 exit 0
